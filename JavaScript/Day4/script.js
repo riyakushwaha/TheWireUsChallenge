179e28334c5eccbfb2a475855158a7f3f4c0e16c
riyakushwaha <reakushwaha@gmail.com>
2020-09-21 18:26:53 +0530

    Day4

---
+++ JavaScript/Day4/script.js
@@ -0,0 +1,6 @@
+
+function change(){
+    document.getElementById("cartoon").style.visibility="hidden";
+    document.getElementById("on").innerHTML="You are Awesome";
+    document.getElementsByTagName("h1")[0].innerHTML="Bye, People";
+}
